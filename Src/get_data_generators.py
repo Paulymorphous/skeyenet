7cdd456e726bce1e7da7489068ee3155b353cdf7
Jerin Paul <42616760+Paulymorphous@users.noreply.github.com>
2020-07-26 23:12:42 +0530

    v2.0.0

--- Src/get_data_generators.py
+++ Src/get_data_generators.py
@@ -9,50 +9,70 @@ Website: https://www.livetheaiexperience.com/
 
 from keras.preprocessing.image import ImageDataGenerator
 
-def GetDataGenerators(train_images_path=None, train_targets_path=None, test_images_path=None, test_targets_path=None, batch_size = 64, seed=42):
-	"""
-	Builds and returns ImageDataGenerators based on the paths that are sepcified. 
+def GetDataGenerators(augmentation_parameters, train_images_path=None, train_targets_path=None, test_images_path=None, test_targets_path=None, batch_size = 64, seed=42):
+    """
+        Builds and returns ImageDataGenerators based on the paths that are sepcified. 
 
-    Please note:
-	> Since this is not a multi-class classification problem and ImageDataGenerators require atleast one folder with images in it, we provide the path to the parent directory of the folder containting the images.
-	> If you want only one type of data generator, then you need to provide the path only for that dataset. See Example for clarification.
-	> A Validation generator is also returned with the Train generator.
-	> You can add more variations to the image by adding more number of augmnetation methods to the Generator.
-	
-    Read more about the prerocessing methods here: https://keras.io/api/preprocessing/image/
+        Please note:
+        > Since this is not a multi-class classification problem and ImageDataGenerators require atleast one folder with images in it, we provide the path to the parent directory of the folder containting the images.
+        > If you want only one type of data generator, then you need to provide the path only for that dataset. See Example for clarification.
+        > A Validation generator is also returned with the Train generator.
+        > You can modify the parameters for Image Augmentation in the Config File.
 
-	Parameters
-   	----------
-	>train_images_path (str): Path to the parent folder of the folder containting Train Images.
-	>train_targets_path (str): Path to the parent folder of the folder containting Train Masks.
-	>test_images_path (str): Path to the parent folder of the folder containting Test Images.
-	>test_targets_path (str): Path to the parent folder of the folder containting Test Masks.
-	>batch_size (int): Desired batch size for the datagenerators. Default: 64.
-	>seed (int): this number seeds the Datagenerators. Default: 42, because its the answer to everything ;)
+        Read more about the prerocessing methods here: https://keras.io/api/preprocessing/image/
 
-	Returns
-   	----------
-	> A list which can have some or all of the three types of Datagenerators - Train data generator, validation data generator, or/and Test data generator.
+        Parameters
+        ----------
+        >augmentation_parameters (Object of ConfigParser SectionProxy): Configurations from for Augmentation.
+        >train_images_path (str): Path to the parent folder of the folder containting Train Images.
+        >train_targets_path (str): Path to the parent folder of the folder containting Train Masks.
+        >test_images_path (str): Path to the parent folder of the folder containting Test Images.
+        >test_targets_path (str): Path to the parent folder of the folder containting Test Masks.
+        >batch_size (int): Desired batch size for the datagenerators. Default: 64.
+        >seed (int): this number seeds the Datagenerators. Default: 42, because its the answer to everything ;)
 
-	Example
-   	----------
-	> If you want to test your model and only require the Test Datagenerator:
-	  	test_generator = GetDataGenerators(test_images_path= <Path to test Images>,
-		  								   test_targets_path= <Path to test Maskss>,
-										   batch_size = 128
-										   )							   
-	"""
+        Returns
+        ----------
+        > A list which can have some or all of the three types of Datagenerators - Train data generator, validation data generator, or/and Test data generator.
+
+        Example
+        ----------
+        > If you want to test your model and only require the Test Datagenerator:
+            test_generator = GetDataGenerators(test_images_path= <Path to test Images>,
+                                            test_targets_path= <Path to test Maskss>,
+                                            batch_size = 128
+                                            )
+    """
 
     generators = []
-    
+
+    def get_boolean(string):
+        if string == "True":
+            return True
+        elif string == "False":
+            return False
+       
     if train_images_path and  train_targets_path:
 
         train_datagen = ImageDataGenerator(
-                                           width_shift_range = 0.2,
-                                           height_shift_range = 0.2,
-                                           shear_range = 0.3,
-                                           rescale=1./255,
-                                           validation_split=0.2
+                                            featurewise_center=get_boolean(augmentation_parameters["featurewise_center"]),
+                                            samplewise_center=get_boolean(augmentation_parameters["samplewise_center"]),
+                                            featurewise_std_normalization=get_boolean(augmentation_parameters["featurewise_std_normalization"]),
+                                            samplewise_std_normalization=get_boolean(augmentation_parameters["samplewise_std_normalization"]),
+                                            zca_whitening=get_boolean(augmentation_parameters["zca_whitening"]),
+                                            zca_epsilon=float(augmentation_parameters["zca_epsilon"]),
+                                            rotation_range=float(augmentation_parameters["rotation_range"]),
+                                            width_shift_range=float(augmentation_parameters["width_shift_range"]),
+                                            height_shift_range=float(augmentation_parameters["height_shift_range"]),
+                                            shear_range=float(augmentation_parameters["shear_range"]),
+                                            zoom_range=float(augmentation_parameters["zoom_range"]),
+                                            channel_shift_range=float(augmentation_parameters["channel_shift_range"]),
+                                            fill_mode=augmentation_parameters["fill_mode"],
+                                            cval=float(augmentation_parameters["cval"]),
+                                            horizontal_flip=get_boolean(augmentation_parameters["horizontal_flip"]),
+                                            vertical_flip=get_boolean(augmentation_parameters["vertical_flip"]),
+                                            rescale=float(augmentation_parameters["rescale"]),
+                                            validation_split = float(augmentation_parameters["validation_split"])
                                            )
 
         train_image_generator = train_datagen.flow_from_directory(
@@ -97,7 +117,26 @@ def GetDataGenerators(train_images_path=None, train_targets_path=None, test_imag
     if test_images_path and  test_targets_path:
         
     
-        test_datagen = ImageDataGenerator(rescale=1./255)
+        test_datagen = ImageDataGeneratorImageDataGenerator(
+                                            featurewise_center=get_boolean(augmentation_parameters["featurewise_center"]),
+                                            samplewise_center=get_boolean(augmentation_parameters["samplewise_center"]),
+                                            featurewise_std_normalization=get_boolean(augmentation_parameters["featurewise_std_normalization"]),
+                                            samplewise_std_normalization=get_boolean(augmentation_parameters["samplewise_std_normalization"]),
+                                            zca_whitening=get_boolean(augmentation_parameters["zca_whitening"]),
+                                            zca_epsilon=float(augmentation_parameters["zca_epsilon"]),
+                                            rotation_range=float(augmentation_parameters["rotation_range"]),
+                                            width_shift_range=float(augmentation_parameters["width_shift_range"]),
+                                            height_shift_range=float(augmentation_parameters["height_shift_range"]),
+                                            shear_range=float(augmentation_parameters["shear_range"]),
+                                            zoom_range=float(augmentation_parameters["zoom_range"]),
+                                            channel_shift_range=float(augmentation_parameters["channel_shift_range"]),
+                                            fill_mode=augmentation_parameters["fill_mode"],
+                                            cval=float(augmentation_parameters["cval"]),
+                                            horizontal_flip=get_boolean(augmentation_parameters["horizontal_flip"]),
+                                            vertical_flip=get_boolean(augmentation_parameters["vertical_flip"]),
+                                            rescale=float(augmentation_parameters["rescale"]),
+                                            validation_split = float(augmentation_parameters["validation_split"])
+                                           )
 
 
         test_image_generator = test_datagen.flow_from_directory(
---
+++ Src/loss_functions.py
@@ -0,0 +1,10 @@
+import tensorflow.keras.backend as K
+
+def dice_coef(y_true, y_pred, smooth = 1):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+def soft_dice_loss(y_true, y_pred):
+    return 1-dice_coef(y_true, y_pred)
